9a464c792199a7917e2218ccee7d7a85c1a14271
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-04-11 07:47:15 +0530

    baseclass

---
+++ src/test/java/testbase/baseclass.java
@@ -0,0 +1,45 @@
+package testbase;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import utilities.getdatafromconfig;
+
+import java.time.Duration;
+
+public class baseclass extends getdatafromconfig {
+    public WebDriver driver;
+    String browserfortest=get_browser("browser");
+    String linkforluanchportal=get_testlink("link");
+    @BeforeClass
+
+    public void launch()
+    {
+
+        if(browserfortest.equalsIgnoreCase("chrome"))
+        {
+            driver=new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get(linkforluanchportal);
+        } else if (browserfortest.equalsIgnoreCase("firefox")) {
+            driver=new FirefoxDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get(linkforluanchportal);
+        }else {
+            driver=new EdgeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.get(linkforluanchportal);
+        }
+    }
+    @AfterClass
+    public void tear_down(){
+        driver.quit();
+    }
+}
