47ef3b9b1c5c17f928ec238f289a8c9cf6a64cd4
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-04-13 09:18:30 +0530

    new base class with parameters added

--- src/test/java/testbase/baseclass.java
+++ src/test/java/testbase/baseclass.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import utilities.getdatafromconfig;
 
@@ -16,37 +17,59 @@ import java.time.Duration;
 public class baseclass extends getdatafromconfig {
     public WebDriver driver;
     public Logger logger;
-    String browserfortest=get_browser("browser");
-    String linkforluanchportal=get_testlink("link");
+    //String browserfortest=get_browser("browser");
+    String linkforluanchportal = get_testlink("link");
+
     @BeforeClass
-
-    public void launch()
-    {
-
-        if(browserfortest.equalsIgnoreCase("chrome"))
-        {
-            logger = LogManager.getLogger(this.getClass());
-            driver=new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(linkforluanchportal);
-        } else if (browserfortest.equalsIgnoreCase("firefox")) {
-            driver=new FirefoxDriver();
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(linkforluanchportal);
-        }else {
-            driver=new EdgeDriver();
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.get(linkforluanchportal);
+    @Parameters("browser")
+    public void launch(String browser) {
+        logger = LogManager.getLogger(this.getClass());
+        switch (browser.toLowerCase()) {
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                System.out.println("invalid browser name");
+                return;
         }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.get(linkforluanchportal);
     }
+
     @AfterClass
-    public void tear_down(){
+    public void tear_down() {
         driver.quit();
     }
 }
+
+
+//if(browserfortest.equalsIgnoreCase("chrome"))
+//{
+//   logger = LogManager.getLogger(this.getClass());
+//  driver=new ChromeDriver();
+//  driver.manage().window().maximize();
+//  driver.manage().deleteAllCookies();
+//  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+// driver.get(linkforluanchportal);
+// } else if (browserfortest.equalsIgnoreCase("firefox")) {
+// driver=new FirefoxDriver();
+//  driver.manage().window().maximize();
+//   driver.manage().deleteAllCookies();
+//   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//   driver.get(linkforluanchportal);
+// }else {
+//  driver=new EdgeDriver();
+//  driver.manage().window().maximize();
+// driver.manage().deleteAllCookies();
+// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+// driver.get(linkforluanchportal);
+// }
+
+
