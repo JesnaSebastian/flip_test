4b8ef5876680eba3e7b64f6778c17183a6d9bea4
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-05-17 17:14:36 +0530

    login pojo

---
+++ src/test/java/API_Pojo/loginPojo.java
@@ -0,0 +1,16 @@
+package API_Pojo;
+
+public class loginPojo {
+
+   private String email;
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String password;
+}
