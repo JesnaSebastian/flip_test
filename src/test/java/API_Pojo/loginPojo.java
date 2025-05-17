b7bd254c6e22bdf1d7e034a28fbf1246b2ba4372
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-05-17 17:40:53 +0530

    added getter

--- src/test/java/API_Pojo/loginPojo.java
+++ src/test/java/API_Pojo/loginPojo.java
@@ -2,15 +2,22 @@ package API_Pojo;
 
 public class loginPojo {
 
-   private String email;
+    private String email;
+    private String password;
 
-    public void setPassword(String password) {
-        this.password = password;
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    private String password;
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
