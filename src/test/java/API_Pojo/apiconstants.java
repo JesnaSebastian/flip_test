e4f28522e90a024666c55b30d4ff47bba5cef1f7
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-05-17 17:05:50 +0530

    constants

---
+++ src/test/java/API_Pojo/apiconstants.java
@@ -0,0 +1,7 @@
+package API_Pojo;
+
+public class apiconstants {
+    final static String baseUrl ="https://thinking-tester-contact-list.herokuapp.com";
+    final static String loginEndPoint = "/users/login";
+    final static String addContact= "/contacts";
+}
